d5c4d85ed173fec81b8c9bcf30bf9ca8a143d838
Andre Simon <aasimon@uc.cl>
2019-09-05 13:49:29 -0400

    changed remote endpoint

--- E0/client/chat-app/src/App.js
+++ E0/client/chat-app/src/App.js
@@ -6,8 +6,8 @@ import MessageForm from "./components/MessageForm";
 import MessageBubble from './components/MessageBubble';
 import "./App.css";
 
-//const endpoint = "http://ec2-18-222-124-71.us-east-2.compute.amazonaws.com:5000";
-const endpoint = "http://localhost:5000";
+const endpoint = "http://ec2-18-222-124-71.us-east-2.compute.amazonaws.com:5000";
+//const endpoint = "http://localhost:5000";
 const socket = io(endpoint, { transports: ["polling"] });
 
 const App = () => {
